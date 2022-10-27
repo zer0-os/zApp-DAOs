ead3842456e829bfc9381d04b9de5c1fdf70aeab
Brett <12437916+colbr@users.noreply.github.com>
2022-10-27 17:56:45 +1300

    chore: prettier

--- src/lib/constants/networks.ts
+++ src/lib/constants/networks.ts
@@ -1,30 +1,23 @@
 export enum Network {
 	MAINNET = 1,
-	RINKEBY = 4,
 }
 
-export const DEFAULT_NETWORK = Network.RINKEBY;
+export const DEFAULT_NETWORK = Network.MAINNET;
 
 export const DEFAULT_IPFS_GATEWAY = 'snapshot.mypinata.cloud';
 
-export const NETWORK_CONFIGS: Record<
-	Network,
-	{
-		rpcUrl: string;
-		ipfsGateway: string;
-	}
-> = {
+interface NetworkConfig {
+	rpcUrl: string;
+	ipfsGateway: string;
+}
+
+export const NETWORK_CONFIGS: Record<Network, NetworkConfig> = {
 	[Network.MAINNET]: {
 		rpcUrl: 'https://mainnet.infura.io/v3/77c3d733140f4c12a77699e24cb30c27',
-		ipfsGateway: DEFAULT_IPFS_GATEWAY
-	},
-	[Network.RINKEBY]: {
-		rpcUrl: 'https://rinkeby.infura.io/v3/fa959ead3761429bafa6995a4b25397e',
 		ipfsGateway: DEFAULT_IPFS_GATEWAY
 	}
 };
 
 export const NETWORK_ETHERSCAN: Record<Network, string> = {
-	[Network.MAINNET]: '',
-	[Network.RINKEBY]: 'rinkeby.'
+	[Network.MAINNET]: ''
 };
