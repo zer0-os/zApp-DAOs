dcb516bedae54e66402b409d8f83be26c7c48607
dominic <domw30@gmail.com>
2023-03-08 13:10:44 +1100

    refactor: tidy up logic and styles

--- src/features/create-proposal/CreateProposal.tsx
+++ src/features/create-proposal/CreateProposal.tsx
@@ -1,34 +1,44 @@
 import type { FC } from 'react';
-import type { CreateProposalProps } from './CreateProposal.types';
-
-import React, { useLayoutEffect } from 'react';
+import React, { useCallback, useLayoutEffect } from 'react';
 import { useHistory } from 'react-router-dom';
-import { LoadingIndicator } from '@zero-tech/zui/components';
-import { useUserPaymentTokenBalance, useDaoAssets } from '../../lib/hooks';
+
+import { AssetType } from '@zero-tech/zdao-sdk';
 import { DAO_CREATE_PROPOSAL } from '../../pages/DAO/DAO.constants';
-import { BackLinkButton } from '../ui';
+import type { CreateProposalProps } from './CreateProposal.types';
+import {
+	useUserPaymentTokenBalance,
+	useDaoAssets,
+	useWeb3
+} from '../../lib/hooks';
+
+import { BackLinkButton, ConnectWallet } from '../ui';
 import { CreateProposalForm } from './CreateProposalForm';
+import { Button, LoadingIndicator } from '@zero-tech/zui/components';
+
 import styles from './CreateProposal.module.scss';
 
 export const CreateProposal: FC<CreateProposalProps> = ({
 	isLoadingDao,
 	dao
 }) => {
-	// Hooks
+	const { account } = useWeb3();
 	const history = useHistory();
-	const { data: assets } = useDaoAssets(dao);
+	const { data: assets, isLoading: isLoadingAssets } = useDaoAssets(dao);
 	const {
 		isLoading: isLoadingPaymentTokenBalance,
 		data: userPaymentTokenBalance
 	} = useUserPaymentTokenBalance(dao?.votingToken.token);
 
-	// Data
-	const isLoading = isLoadingDao || isLoadingPaymentTokenBalance;
-	const isHoldingVotingToken = !userPaymentTokenBalance?.gt(0);
+	const isLoading =
+		isLoadingDao || isLoadingPaymentTokenBalance || isLoadingAssets;
+	const isHoldingVotingToken = userPaymentTokenBalance?.gt(0);
 	const toAllProposals = history.location.pathname.replace(
 		`/${DAO_CREATE_PROPOSAL}`,
 		''
 	);
+	const isDaoHoldingERC20Asset =
+		Boolean(assets) &&
+		assets.filter((asset) => asset.type === AssetType.ERC20).length > 0;
 
 	useLayoutEffect(() => {
 		const nav = document.getElementById('dao-page-nav-tabs');
@@ -40,38 +50,64 @@ export const CreateProposal: FC<CreateProposalProps> = ({
 		};
 	}, []);
 
-	// Render
+	const handleGoToDao = useCallback(() => {
+		history.replace(toAllProposals);
+	}, [history, toAllProposals]);
+
+	if (!account) {
+		return (
+			<ConnectWallet message={'Connect your wallet to create a proposal.'} />
+		);
+	}
+
 	return (
 		<div className={styles.Container}>
-			{/* Back to All Proposals */}
 			<BackLinkButton label="All Proposals" to={toAllProposals} />
 
-			{/* Content */}
+			<h1 className={styles.Header}>Create a Proposal</h1>
+
 			<div className={styles.Content}>
 				{isLoading && (
 					<LoadingIndicator
 						className={styles.Loading}
-						text="Loading dao"
+						text="Loading DAO"
 						spinnerPosition="left"
 					/>
 				)}
 
-				{!isLoading &&
-					(isHoldingVotingToken ? (
-						<div className={styles.TokenError}>
-							To create a proposal, you need to be holding the {dao.title}{' '}
-							voting token
-							{dao.votingToken.symbol && ` (${dao.votingToken.symbol})`}.
-						</div>
-					) : (
-						<div className={styles.Wrapper}>
-							{/* Title */}
-							<h1 className={styles.Title}>Create a Proposal</h1>
+				{!isLoading && (
+					<>
+						<>
+							{!isHoldingVotingToken && (
+								<div className={styles.TextContent} data-variant={'warning'}>
+									To create a proposal, you need to be holding the {dao.title}{' '}
+									voting token
+									{dao.votingToken.symbol && ` (${dao.votingToken.symbol})`}.
+								</div>
+							)}
 
-							{/* Form */}
-							<CreateProposalForm dao={dao} assets={assets} />
-						</div>
-					))}
+							{!isDaoHoldingERC20Asset && (
+								<>
+									<div className={styles.TextContent} data-variant={'warning'}>
+										You cannot create a funding proposal as this DAO treasury
+										does not hold any tokens to request.
+									</div>
+									<div className={styles.TextContent}>
+										Proposals are currently limited to funding proposals (where
+										DAO ERC20 tokens are sent to a recipient, if the proposal is
+										approved). Please check the assets tab of the DAO.
+									</div>
+								</>
+							)}
+							{(!isHoldingVotingToken || !isDaoHoldingERC20Asset) && (
+								<Button onPress={handleGoToDao}>Back To Dao</Button>
+							)}
+						</>
+					</>
+				)}
+				{isHoldingVotingToken && isDaoHoldingERC20Asset && (
+					<CreateProposalForm dao={dao} assets={assets} />
+				)}
 			</div>
 		</div>
 	);
