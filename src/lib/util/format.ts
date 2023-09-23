ade2025b5cf5a8f34c0f69a056235ff08fa39c78
Brett <12437916+colbr@users.noreply.github.com>
2023-09-23 19:59:09 +1200

    feat: 2dp

--- src/lib/util/format.ts
+++ src/lib/util/format.ts
@@ -15,7 +15,10 @@ export const formatBigNumberString = (str: string, decimalPlaces?: number) => {
 };
 
 export const formatFiat = (amount: number | string) => {
-	return Number(amount).toLocaleString(undefined, { maximumFractionDigits: 2 });
+	return Number(amount).toLocaleString(undefined, {
+		minimumFractionDigits: 2,
+		maximumFractionDigits: 2,
+	});
 };
 
 export const formatPercentage = (percentage: number | string) => {
