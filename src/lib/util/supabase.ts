d25eff8335480ac6ba64243ea01224e10f2cb396
Brett <12437916+colbr@users.noreply.github.com>
2023-12-08 13:26:16 +1300

    chore: prettier

--- src/lib/util/supabase.ts
+++ src/lib/util/supabase.ts
@@ -1,7 +1,10 @@
 import { createClient } from '@supabase/supabase-js';
 
-if(!import.meta.env.VITE_SUPABASE_URL || !import.meta.env.VITE_SUPABASE_ANON_KEY) {
-    throw new Error('Missing Supabase URL or Supabase Anon Key');
+if (
+	!import.meta.env.VITE_SUPABASE_URL ||
+	!import.meta.env.VITE_SUPABASE_ANON_KEY
+) {
+	throw new Error('Missing Supabase URL or Supabase Anon Key');
 }
 
 export const supabase = createClient(
