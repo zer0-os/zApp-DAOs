5ff99ca6480898ea4e8104b5e6d45ef92cca6b7e
Brett <12437916+colbr@users.noreply.github.com>
2023-12-08 13:39:00 +1300

    feat: error handling for failed retrieval

--- src/lib/hooks/queries/useDaoAssetsCoins.ts
+++ src/lib/hooks/queries/useDaoAssetsCoins.ts
@@ -20,39 +20,43 @@ export const useDaoAssetsCoins = (zna?: string) => {
 
 			const data = await response.json();
 
-			if (!Number(data[0].fiatBalance ?? 1)) {
-				const symbols = data.map((d) => {
-					if (!d.token) return 'eth';
-					return d.token.symbol;
-				});
+			try {
+				if (!Number(data[0].fiatBalance ?? 1)) {
+					const symbols = data.map((d) => {
+						if (!d.token) return 'eth';
+						return d.token.symbol;
+					});
 
-				const res = await supabase.functions.invoke(
-					`get_token_price?symbol=${symbols.join(',')}`,
-					{
-						method: 'GET',
-					},
-				);
+					const res = await supabase.functions.invoke(
+						`get_token_price?symbol=${symbols.join(',')}`,
+						{
+							method: 'GET',
+						},
+					);
 
-				const prices = res.data.data;
+					const prices = res.data.data;
 
-				data.forEach((d) => {
-					const price = prices[d.token?.symbol ?? 'ETH']?.filter((p) => {
-						if (!p.platform?.tokenAddress) {
-							return true;
+					data.forEach((d) => {
+						const price = prices[d.token?.symbol ?? 'ETH']?.filter((p) => {
+							if (!p.platform?.tokenAddress) {
+								return true;
+							}
+							return (
+								p.platform.tokenAddress.toLowerCase() ===
+								(d.tokenAddress?.toLowerCase() ?? undefined)
+							);
+						})[0];
+						if (price) {
+							const balanceAsNumber = Number(
+								ethers.utils.formatUnits(d.balance, d.token?.decimals ?? 18),
+							);
+							const priceInUSD = price.quote['USD'].price;
+							d.fiatBalance = balanceAsNumber * priceInUSD;
 						}
-						return (
-							p.platform.tokenAddress.toLowerCase() ===
-							(d.tokenAddress?.toLowerCase() ?? undefined)
-						);
-					})[0];
-					if (price) {
-						const balanceAsNumber = Number(
-							ethers.utils.formatUnits(d.balance, d.token?.decimals ?? 18),
-						);
-						const priceInUSD = price.quote['USD'].price;
-						d.fiatBalance = balanceAsNumber * priceInUSD;
-					}
-				});
+					});
+				}
+			} catch (e) {
+				console.error('Failed to retrieve token prices', e);
 			}
 
 			const coins = data.map((d) => {
